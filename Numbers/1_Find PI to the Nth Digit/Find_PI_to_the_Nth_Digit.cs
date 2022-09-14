425402669193db546bd89f6802f324ca6e0d6937
kami <kami.benli@codeasy.net>
2022-09-14 11:34:30 +0200

    Find Pi to the Nth digit Completed

--- Numbers/1_Find PI to the Nth Digit/Find_PI_to_the_Nth_Digit.cs
+++ Numbers/1_Find PI to the Nth Digit/Find_PI_to_the_Nth_Digit.cs
@@ -4,6 +4,23 @@ public class Find_Pi_to_the_Nth_Digit
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("Hello Mono World");
+		double pi = Math.PI;
+		Console.WriteLine(pi);
+		
+		Console.WriteLine("Which digit of Pi you want to see?");
+		int nthDigit = int.Parse(Console.ReadLine());
+		
+		double piPowered = pi*Math.Pow(10,nthDigit);
+		
+		int integerResult = (int)piPowered;
+		
+		Console.Write("The desired digit of Pi is:");
+		Console.WriteLine(integerResult%10);
+		
+		
+	
+		
+		
+		
     }
 }
